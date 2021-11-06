3058c3d85f53d38fbf0d9bc3f6b5a0f79e92b38b
AlinaTuz <86747591+MellFox@users.noreply.github.com>
2021-11-06 19:06:33 +0200

    Create Temperature.cs

---
+++ Temperature.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuz.Lab03.Task03.ClassLib
+{
+	public enum Temperature
+	{
+		High,
+		Middle,
+		Low
+	}
+}
