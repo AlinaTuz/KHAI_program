eb73cf63a063fcb19aa5c2cf2ebd295e70b698ff
AlinaTuz <86747591+MellFox@users.noreply.github.com>
2021-11-06 19:05:29 +0200

    Create Resolution.cs

---
+++ Resolution.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuz.Lab03.Task03.ClassLib
+{
+	public enum Resolution
+	{
+		HD, 
+		UHD
+	}
+}
