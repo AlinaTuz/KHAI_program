f776cc24e7c7e9a666b0fe46459cf46d830f7a74
AlinaTuz <86747591+MellFox@users.noreply.github.com>
2021-11-06 19:06:04 +0200

    Create Steam.cs

---
+++ Steam.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuz.Lab03.Task03.ClassLib
+{
+	public enum Steam
+	{
+		Supply,
+		Hit,
+		SelfCleaning,
+		Steaming
+	}
+}
